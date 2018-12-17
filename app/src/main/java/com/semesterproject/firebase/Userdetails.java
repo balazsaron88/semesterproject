89c6537496004a13c638af62bc180668dd0ae9b6
Balazs Aron <balazsaron88@gmail.com>
2018-12-17 23:37:43 +0200

    - added Firebase database and displaying in recycleview

---
+++ app/src/main/java/com/semesterproject/firebase/Userdetails.java
@@ -0,0 +1,37 @@
+package com.semesterproject.firebase;
+
+/**
+ * Created by csa on 3/6/2017.
+ */
+
+public class Userdetails {
+
+    public String name;
+    public String email;
+    public String address;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
